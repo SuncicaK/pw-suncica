27aea34262f74d7359a9ddc05b02088298e24bc5
SuncicaK <suncica772@gmail.com>
2026-04-12 15:34:13 +0200

    added global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
   reporter: 'html',
+   timeout: 60000,
   use: {
     baseURL: 'https://www.demoblaze.com',
     trace: 'on-first-retry',
