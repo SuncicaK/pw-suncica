59043f9f69c2818fdd75ec9e920a2dc940be9681
SuncicaK <suncica772@gmail.com>
2026-06-06 10:12:32 +0200

    cache playwright browsers in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
-  reporter: 'list',
+  reporter: '',
   timeout: 60000,
   use: {
     baseURL: 'https://www.demoblaze.com',
